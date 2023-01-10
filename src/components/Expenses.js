843b6482678c0490cf08ddde42f27fd933b9d100
nissevahavahe <e2101563@edu.vamk.fi>
2023-01-10 14:04:25 +0200

    Muokkaus  jossa expenses komponenttiä lyhenettiin

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,24 +1,19 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem.js';
 function Expenses(props){
+
+    const object = props.expenses.map((items, index) =>(
+        <ExpenseItem
+            key={index}
+            title={items.title}
+            amount={items.amount}
+            date={items.date}/>
+
+    ))
+    
     return(
         <div className="expenses">
-        <ExpenseItem
-        title = {props.expenses[0].title}
-        date = {props.expenses[0].date}
-        amount = {props.expenses[0].amount}/>
-        <ExpenseItem
-        title = {props.expenses[1].title}
-        date = {props.expenses[1].date}
-        amount = {props.expenses[1].amount}/>
-        <ExpenseItem
-        title = {props.expenses[2].title}
-        date = {props.expenses[2].date}
-        amount = {props.expenses[2].amount}/>
-        <ExpenseItem
-        title = {props.expenses[3].title}
-        date = {props.expenses[3].date}
-        amount = {props.expenses[3].amount}/>
+            {object}
         </div>
     )
 }
