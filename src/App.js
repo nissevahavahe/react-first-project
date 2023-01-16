490810c7ae05b02afc81d0536a9310af33fd2c5b
nissevahavahe <e2101563@edu.vamk.fi>
2023-01-16 14:21:23 +0200

    Muokattu expense objektia

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date("2022-07-14"),
+      date: new Date("2023-01-12"),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-12") },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-01") },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 300.00,
-      date: new Date("2023-01-01"),
+      date: new Date("2022-07-14"),
     },
     {
       id: 'e4',
