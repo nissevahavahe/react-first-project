de4bff262bafb929bbf2bc921f3564f3de071ffb
nissevahavahe <e2101563@edu.vamk.fi>
2023-01-17 11:00:48 +0200

    Lisätty expense filtterin alkulogiikka

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem.js';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+
 const Expenses = (props) =>{
 
     const object = props.expenses.map((items, index) =>(
@@ -12,9 +14,16 @@ const Expenses = (props) =>{
             date={items.date}/>
 
     ))
+    const [filteredYear, setFilteredYear] = useState('2023');
+    const filterChangeHandler = (selectedYear) => {
+        //console.log('Expenses.js')
+        //console.log(selectedYear);
+        setFilteredYear(selectedYear)
+    }
     
     return(
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {object}
         </Card>
     )
